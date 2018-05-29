2aacb0f6de0bd53d694490a4b635f83b5d6da624
lupuuss <damian.baczynski@gmail.com>
2018-05-29 19:20:02 +0200

    Auto login implementation

--- app/src/main/java/ga/lupuss/anotherbikeapp/ui/StartActivity.kt
+++ app/src/main/java/ga/lupuss/anotherbikeapp/ui/StartActivity.kt
@@ -6,6 +6,7 @@ import android.os.Handler
 import ga.lupuss.anotherbikeapp.AnotherBikeApp
 import ga.lupuss.anotherbikeapp.R
 import ga.lupuss.anotherbikeapp.base.BaseActivity
+import ga.lupuss.anotherbikeapp.models.User
 import ga.lupuss.anotherbikeapp.ui.modules.login.LoginActivity
 import ga.lupuss.anotherbikeapp.ui.modules.main.MainActivity
 
@@ -18,10 +19,24 @@ class StartActivity : BaseActivity() {
         Handler().postDelayed({
 
             val intent: Intent =
-                    if (AnotherBikeApp.get(this.application).mainComponent == null) {
-                        LoginActivity.newIntent(this)
-                    } else {
+
+                    if (AnotherBikeApp.get(this.application).mainComponent != null) {
+
                         MainActivity.newIntent(this)
+
+                    } else {
+
+                        val user = AnotherBikeApp.get(this.application)
+                                .coreComponent.providesFirebaseAuth().currentUser
+
+                        if (user != null) {
+                            AnotherBikeApp.get(this.application)
+                                    .initMainComponentWithUser(User(user, this))
+                            MainActivity.newIntent(this)
+
+                        } else {
+                            LoginActivity.newIntent(this)
+                        }
                     }
             finish()
             startActivity(intent)
