3de7cba42bc116a2faef42ecc58911a408c0f9da
lupuuss <damian.baczynski@gmail.com>
2018-06-01 21:40:28 +0200

    Removes unused methods

--- app/src/main/java/ga/lupuss/anotherbikeapp/base/Presenter.kt
+++ app/src/main/java/ga/lupuss/anotherbikeapp/base/Presenter.kt
@@ -4,13 +4,8 @@ package ga.lupuss.anotherbikeapp.base
 /** Base interface for presenters */
 interface Presenter {
 
-    fun initWithStateJson(stateJson: String?) {}
-
     fun notifyOnViewReady() {}
-
     fun notifyOnResult(requestCode: Int, resultCode: Int) {}
-
     fun notifyOnDestroy(isFinishing: Boolean) {}
 
-    fun getStateJson(): String? = null
 }
