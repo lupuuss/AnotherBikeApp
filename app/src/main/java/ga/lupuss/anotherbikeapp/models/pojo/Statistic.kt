0cc19594de026f6a080e9cf187074b70176ff828
lupuuss <damian.baczynski@gmail.com>
2018-06-01 17:58:18 +0200

    Statistics refactor cont.

---
+++ app/src/main/java/ga/lupuss/anotherbikeapp/models/pojo/Statistic.kt
@@ -0,0 +1,44 @@
+package ga.lupuss.anotherbikeapp.models.pojo
+
+import ga.lupuss.anotherbikeapp.models.trackingservice.statisticsmanager.Status
+
+/**
+ * Base class for any statistic.
+ */
+sealed class Statistic<T> {
+    /** Names of possible statistics */
+    enum class Name {
+
+        SPEED,
+        AVG_SPEED,
+        MAX_SPEED,
+        DISTANCE,
+        DURATION,
+        STATUS,
+        START_TIME;
+    }
+
+    /**
+     * Represent units used in app.
+     *
+     * @param convertParam is used to convert SI units to others
+     * e.g 3.6 km/h is 1 m/s so convertParam is 3.6
+     */
+    enum class Unit(val convertParam: Double) {
+        M_S(1.0), // SI unit
+        KM_H( 3.6),
+        M( 1.0), // SI unit
+        KM(0.001)
+    }
+
+    abstract val value: T
+}
+
+/** [Statistic] subclass that can contain [value] as double.
+ * [value] might be converted to any [unit].
+ * */
+
+class UnitStatistic(override val value: Double, val unit: Unit) : Statistic<Double>()
+class TimeStatistic(override val value: Long) : Statistic<Long>()
+class StringStatistic(override val value: String) : Statistic<String>()
+class StatusStatistic(override val value: Status) : Statistic<Status>()
